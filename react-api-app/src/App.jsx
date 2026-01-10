7fefb390984378a8b1c3e8d38ae9d863e43d939e
Viresh <kariappavbk123@gmail.com>
2026-01-10 12:28:31 +0530

    API Handling

--- react-api-app/src/App.jsx
+++ react-api-app/src/App.jsx
@@ -13,13 +13,45 @@ function App() {
   useEffect(()=>{
     // fetch data from api
     fetch('https://jsonplaceholder.typicode.com/users')
+    // response handling
     .then(response=>{
       if(!response.ok){
         throw new Error('Network response was not ok');
       }
       return response.json();
     })
-  })
+    // data handling
+    .then(data=>{
+      setUsers(data);
+      setLoading(false);
+    })
+    // error handling
+    .catch(error=>{
+      setError(error.message);
+      setLoading(false);
+    })
+  },[]);
+
+  return(
+    <div style={{padding:"20px"}}>
+      <h1>API Integeration</h1>
+      {/* Loading */}
+      {loading && <p>Loading...</p>}
+
+      {/* Error */}
+      {error && <p style={{color:'red'}}>Error: {error}</p>}
+
+      {/* Data */}
+      <ul>
+        {users.map(user=>(
+          <li key={user.id}>
+            <strong>{user.name}</strong> - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+
+  )
 }
 
 export default App
